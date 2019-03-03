1a412ce33d983a81a4c1f52ec150deff01fac36d
Ghislain Nono Gueye <nonoghislain@gmail.com>
2019-03-03 04:41:20 -0600

    Added data for vignette.

---
+++ R/scores_data.R
@@ -0,0 +1,14 @@
+#' Evaluation scores for students.
+#' 
+#' A dataset containing scores on quizzes, homework assignments, exams and
+#' attendance for a hypothetical class of 30 students.
+#' 
+#' @format A tibble (\code{tbl}) of 30 rows and 11 columns:
+#' \describe{
+#'     \item{student}{first name, last name}
+#'     \item{quiz_1, quiz_2, quiz_3}{quiz scores out of 20}
+#'     \item{homework_1, homework_2, homework_3}{homework scores out of 50}
+#'     \item{exam_1, exam_2, exam_3, final}{exam scores out of 100}#' 
+#' }
+
+"scores"
