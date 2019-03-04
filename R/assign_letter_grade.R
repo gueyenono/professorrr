f056063d779d8ba237c2b50957b3df99099c001a
Ghislain Nono Gueye <nonoghislain@gmail.com>
2019-03-04 03:47:48 -0600

    Corrected typo in the assign_letter_grade() function.

--- R/assign_letter_grade.R
+++ R/assign_letter_grade.R
@@ -3,26 +3,26 @@
 #' Assign a letter grade based on the grade computed by the \code{compute_grade} function.
 #'
 #' @param x A \code{tbl_prof} object.
-#' @param intervals A numeric vector specifying intervals for grades.
-#' @param letter_grades A character vector of letter grades.
+#' @param interval A numeric vector specifying interval for grades.
+#' @param letter_grade A character vector of letter grades.
 #' @return a \code{tbl_prof} object.
 #' 
 #' @importFrom dplyr bind_cols %>%
 #' @export
 
-assign_letter_grade <- function(x, intervals = NULL, letter_grades = NULL){
+assign_letter_grade <- function(x, interval = NULL, letter_grade = NULL){
 
-	if(is.null(intervals)) intervals <- c(-Inf, 60, 70, 80, 90, Inf)
+	if(is.null(interval)) interval <- c(-Inf, 60, 70, 80, 90, Inf)
 	if(is.null(letter_grade)) letter_grade <- c("F", "D", "C", "B", "A")
 
 	total <- attr(x, "total")
 
-	grades <- cut(total, intervals, letter_grades, include.lowest = TRUE, right = FALSE)
+	grades <- cut(total, interval, letter_grade, include.lowest = TRUE, right = FALSE)
 	out <- dplyr::bind_cols(x, grades)
 
 	if(!is.null(attr(x, "total_dropped"))){
 		total_dropped <- attr(x, "total_dropped")
-		grades_dropped <- cut(total_dropped, intervals, letter_grades, include.lowest = TRUE, right = FALSE)
+		grades_dropped <- cut(total_dropped, interval, letter_grade, include.lowest = TRUE, right = FALSE)
 		out <- dplyr::bind_cols(out, grades_dropped)
 	}
 
